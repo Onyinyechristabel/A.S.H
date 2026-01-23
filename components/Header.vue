d7ce3ce3c67a2f90a7ee3b5355aab807a337cf93
Onyinye <onyinye241381@gmail.com>
2026-01-23 12:37:20 +0100

    first commit

--- components/Header.vue
+++ components/Header.vue
@@ -1,3 +1,17 @@
 <template>
-  <header class="header"></header>
+  <header class="header">
+    <div>
+      <h3 class="header_text">WORK</h3>
+      <h3 class="header_text">ABOUT</h3>
+      <img class="menuimg" data-mobile src="/images/icon2.svg" alt="" />
+    </div>
+    <div>
+      <img src="/images/artsvg.svg" alt="" />
+    </div>
+    <div>
+      <img class="questionimg" data-mobile src="/images/icon1.svg" alt="" />
+      <h3 class="header_text">SERVICES</h3>
+      <h3 class="header_text">CONTACT US</h3>
+    </div>
+  </header>
 </template>
--- components/Hero.vue
+++ components/Hero.vue
@@ -1,3 +1,19 @@
 <template>
-  <section class="hero"></section>
+  <section class="hero">
+    <div class="video-bg">
+      <video class="video_bgg" autoplay muted loop playsinline>
+        <source src="/images/animeee.mp4" />
+      </video>
+    </div>
+    <div class="hero_content">
+      <div class="hero_firstcontent">
+        <h3>VIDEOS</h3>
+        <div></div>
+        <h3>REPLICAS</h3>
+        <div></div>
+        <h3>EVENTS</h3>
+      </div>
+      <h2>WE BRING YOUR WORLD TO LIFE</h2>
+    </div>
+  </section>
 </template>
