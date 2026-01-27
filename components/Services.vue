b39268b68120d57b99420e86aac46516b5dbb85a
Onyinye <onyinye241381@gmail.com>
2026-01-27 21:13:15 +0100

    update

--- components/Services.vue
+++ components/Services.vue
@@ -1,22 +1,96 @@
+<script setup lang="ts">
+import Button from "./Button.vue";
+</script>
+
 <template>
   <section class="services">
     <h1>Our Services</h1>
     <div class="services_services">
-      <div>
-        <div class="services_first">
+      <div class="first_services">
+        <div class="first_services_left">
           <div>
-            <div
-              class="
-            "
-            ></div>
+            <p>[01]</p>
+            <h2>VIDEO PRODUCTION</h2>
           </div>
+          <h4>
+            Branded content. Live-action. Dev diaries. "Making of" featurettes.
+            Social Content.
+          </h4>
+          <h3>
+            This is a taste of what we can offer. We pride ourselves in helping
+            studios create quality video content that is true to the soul of a
+            game and impactful to its players.
+          </h3>
+          <Button class="first_button" />
         </div>
-        <div class="services_second"></div>
-        <div></div>
+        <div class="first_services_right">
+          <img src="" alt="" />
+        </div>
       </div>
-      <div></div>
-      <div></div>
-      <div></div>
+      <div class="second_services">
+        <div class="second_services_left">
+          <img src="" alt="" />
+        </div>
+        <div class="second_services_right">
+          <div>
+            <p>[02]</p>
+            <h2>HIGH-IMPACT MEDIA</h2>
+          </div>
+          <h4>
+            <span>Focus:</span> Reaching and engaging players through our
+            community of 7M+ followers.
+          </h4>
+          <h4>
+            <span>Value: </span> Increased game awareness, player acquisition,
+            and launch success.
+          </h4>
+          <h4>
+            <span>Expertise: </span> Deep understanding of the industry
+            landscape and target audience segmentation.
+          </h4>
+          <Button />
+        </div>
+      </div>
+      <div class="third_services">
+        <div class="third_services_left">
+          <div>
+            <p>[03]</p>
+            <h2>Immersive Events</h2>
+          </div>
+          <h4>
+            Thanks to our partnership with Polar Pallas Productions, we
+            transform your games into a living, breathing world.
+          </h4>
+          <h3>
+            Whether you're launching a new title, hosting an activation, or
+            celebrating a milestone, we design events that align with your
+            specific objectives and resonate with both your player base and your
+            community.
+          </h3>
+          <Button />
+        </div>
+        <div class="third_services_right">
+          <img src="" alt="" />
+        </div>
+      </div>
+      <div class="forth_services">
+        <div class="forth_services_left">
+          <img src="" alt="" />
+        </div>
+        <div class="forth_services_right">
+          <div>
+            <p>[04]</p>
+            <h2>Custom Products/Merchandising</h2>
+          </div>
+          <h4>
+            Through us, you can create high-end replicas of in-game weapons,
+            character statues, wearable armor, limited-edition collectibles,
+            branded art pieces.
+          </h4>
+
+          <Button class="services_button" />
+        </div>
+      </div>
     </div>
   </section>
 </template>
